e0457e13274777f0b605983dd90ee931f47ce810
Phạm Đức <minhduc.096.99@gmail.com>
2020-02-12 15:11:08 +0700

    .

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import controller from './controller';
 import './components/loading.tag';
 import './components/navbar.tag';
 import './components/footerComponent.tag';
+import { unzipSync } from 'zlib';
 
 const root = document.getElementById("root");
 
@@ -31,9 +32,27 @@ service.checkAuth().then(user => {
     navbar[0].update();
   }
 });
+
+route.parser(function (path) {
+  var raw = path.slice(2).split('?'),
+    uri = raw[0].split('/'),
+    qs = raw[1],
+    params = {}
+
+  if (qs) {
+    qs.split('&').forEach(function (v) {
+      var c = v.split('=')
+      params[c[0]] = c[1]
+    })
+  }
+  uri.push(params);
+  return uri;
+});
+
 route("/detail/*", controller.detailController(riot, root, navbar));
 route("/upload", controller.uploadController(riot, root, navbar));
 route("/signUp", controller.signUp(riot, root));
 route("/signIn", controller.signIn(riot, root));
 route("/..", controller.appController(riot, root, navbar));
+
 route.start(true);
--- src/mx.js
+++ src/mx.js
@@ -32,4 +32,5 @@ function waitFor(seconds) {
   });
 }
 
+
 export default initModal;
